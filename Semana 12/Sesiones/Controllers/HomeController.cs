d5ee5a5463895a28a17dbc1c4b6e0b9a7f6ac9f7
Jonathan Chavez <jce.jonathan@gmail.com>
2018-11-05 17:30:19 -0500

    Implementado sesiones

---
+++ Semana 12/Sesiones/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sesiones.Models;
+
+namespace Sesiones.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            HttpContext.Session.SetString("usuario", "abc");
+
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            var usuario = HttpContext.Session.GetString("usuario");            
+
+            ViewData["Message"] = $"Your contact page. {usuario}";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
